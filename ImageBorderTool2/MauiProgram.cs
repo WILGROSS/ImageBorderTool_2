b9f69f374441412e875d86353bcb3ecc5daf5673
Fittgross <william-gross@hotmail.com>
2024-08-30 00:13:57 +0200

    Started working on color picker

--- ImageBorderTool2/MauiProgram.cs
+++ ImageBorderTool2/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace ImageBorderTool2
 {
@@ -9,6 +10,7 @@ namespace ImageBorderTool2
 			var builder = MauiApp.CreateBuilder();
 			builder
 				.UseMauiApp<App>()
+				.UseMauiCommunityToolkit()
 				.ConfigureFonts(fonts =>
 				{
 					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
