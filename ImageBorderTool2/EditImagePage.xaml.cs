7ca4c5187332a5ef591d369c1babff29d74db84f
WILGROSS <william-gross@hotmail.com>
2024-09-03 13:28:06 +0200

    Finally fixed bug where colors wouldn't transfer correcctly between pages

--- ImageBorderTool2/EditImagePage.xaml.cs
+++ ImageBorderTool2/EditImagePage.xaml.cs
@@ -1,170 +1,182 @@
+using CommunityToolkit.Maui.Core.Extensions;
 using SkiaSharp;
+using System.Diagnostics;
 
 namespace ImageBorderTool2;
 
 public partial class EditImagePage : ContentPage
 {
-	private int _currentImageNumber;
-	private List<string> _imagePaths;
-	private string _imagePath;
-	private int _imageWidth;
-	private int _imageHeight;
+    private int _currentImageNumber;
+    private List<string> _imagePaths;
+    private string _imagePath;
+    private int _imageWidth;
+    private int _imageHeight;
+    private bool _setupComplete;
 
-	private int _currentRed;
-	private int _currentGreen;
-	private int _currentBlue;
-	private int _currentBorderThickness;
+    private Color _currentColor;
+    private Color _debugColor;
+    private int _currentBorderThickness;
 
-	public EditImagePage(List<string> imagePaths, int currentImageNumber, int currentBorderThickness, int red, int green, int blue)
-	{
-		InitializeComponent();
+    public EditImagePage(List<string> imagePaths, int currentImageNumber, int currentBorderThickness, Color currentColor)
+    {
+        InitializeComponent();
 
-		_currentImageNumber = currentImageNumber;
-		_imagePaths = imagePaths;
-		_imagePath = _imagePaths[_currentImageNumber];
+        _setupComplete = false;
+        _currentImageNumber = currentImageNumber;
+        _imagePaths = imagePaths;
+        _imagePath = _imagePaths[_currentImageNumber];
 
-		_currentBorderThickness = currentBorderThickness;
+        _currentBorderThickness = currentBorderThickness;
 
-		_currentRed = red;
-		_currentGreen = green;
-		_currentBlue = blue;
+        _currentColor = currentColor;
+        _debugColor = _currentColor;
 
-		SetCurrentImage();
-		SetupNavigationButtons();
-		SetupPage();
-	}
+        SetCurrentImage();
+        SetupNavigationButtons();
+        SetupPage();
+        SetColorsToPage();
+        _setupComplete = true;
+    }
 
-	private async Task<(int width, int height)> GetImageDimensionsAsync(string imagePath)
-	{
-		using var stream = File.OpenRead(imagePath);
-		var image = SKBitmap.Decode(stream);
+    private async Task<(int width, int height)> GetImageDimensionsAsync(string imagePath)
+    {
+        using var stream = File.OpenRead(imagePath);
+        var image = SKBitmap.Decode(stream);
 
-		return (image.Width, image.Height);
-	}
+        return (image.Width, image.Height);
+    }
 
-	private async void SetCurrentImage()
-	{
-		CurrentImage.Source = _imagePath;
-		(_imageWidth, _imageHeight) = await GetImageDimensionsAsync(_imagePath);
-	}
-	private async void SetupPage()
-	{
-		BorderThicknessSlider.Value = _currentBorderThickness;
-		BorderThicknessLabel.Text = $"{_currentBorderThickness} px";
-		PreviewFrame.Padding = new Thickness(_currentBorderThickness);
+    private async void SetCurrentImage()
+    {
+        CurrentImage.Source = _imagePath;
+        (_imageWidth, _imageHeight) = await GetImageDimensionsAsync(_imagePath);
+    }
+    private async void SetupPage()
+    {
+        BorderThicknessSlider.Value = _currentBorderThickness;
+        BorderThicknessLabel.Text = $"{_currentBorderThickness} px";
+        PreviewFrame.Padding = new Thickness(_currentBorderThickness);
 
-		RedSlider.Value = _currentRed;
-		GreenSlider.Value = _currentGreen;
-		BlueSlider.Value = _currentBlue;
+        RedSlider.Value = _currentColor.GetByteRed();
+        GreenSlider.Value = _currentColor.GetByteGreen();
+        BlueSlider.Value = _currentColor.GetByteBlue();
 
-		ColorPreview.Color = Color.FromRgb(_currentRed, _currentGreen, _currentBlue);
-		PreviewFrame.BackgroundColor = ColorPreview.Color;
+        ColorValueEntry.Text = $"{_currentColor.GetByteRed()},{_currentColor.GetByteGreen()},{_currentColor.GetByteBlue()}";
+    }
 
-		UpdateColorValueEntry();
-	}
+    private void SetColorsToPage()
+    {
+        ColorPreview.Color = _currentColor;
+        PreviewFrame.BackgroundColor = _currentColor;
+    }
 
-	private void SetupNavigationButtons()
-	{
-		if (_imagePath == _imagePaths.First())
-		{
-			PreviousPictureButton.IsVisible = false;
-			FakePreviousPictureButton.IsVisible = true;
-		}
+    private void SetupNavigationButtons()
+    {
+        if (_imagePath == _imagePaths.First())
+        {
+            PreviousPictureButton.IsVisible = false;
+            FakePreviousPictureButton.IsVisible = true;
+        }
 
-		if (_imagePath == _imagePaths.Last())
-		{
-			NextPictureButton.IsVisible = false;
-			FakeNextPictureButton.IsVisible = true;
-		}
-	}
+        if (_imagePath == _imagePaths.Last())
+        {
+            NextPictureButton.IsVisible = false;
+            FakeNextPictureButton.IsVisible = true;
+        }
+    }
 
-	private void UpdateColorValueEntry()
-	{
-		ColorValueEntry.Text = $"{(int)RedSlider.Value},{(int)GreenSlider.Value},{(int)BlueSlider.Value}";
-	}
+    private void UpdateColorValueEntry()
+    {
+        if (!_setupComplete)
+            return;
 
-	private void OnColorValueEntryTextChanged(object sender, TextChangedEventArgs e)
-	{
-		if (string.IsNullOrWhiteSpace(e.NewTextValue))
-			return;
+        ColorValueEntry.Text = $"{(int)RedSlider.Value},{(int)GreenSlider.Value},{(int)BlueSlider.Value}";
+    }
 
-		var parts = e.NewTextValue.Split(',');
-		if (parts.Length == 3 &&
-			int.TryParse(parts[0], out int red) &&
-			int.TryParse(parts[1], out int green) &&
-			int.TryParse(parts[2], out int blue))
-		{
-			if (red < 0 || red > 255 || green < 0 || green > 255 || blue < 0 || blue > 255)
-				return;
+    private void OnColorValueEntryTextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (!_setupComplete)
+            return;
 
-			SetColors(red, green, blue);
+        if (string.IsNullOrWhiteSpace(e.NewTextValue))
+            return;
 
-			RedSlider.Value = _currentRed;
-			GreenSlider.Value = _currentGreen;
-			BlueSlider.Value = _currentBlue;
+        var parts = e.NewTextValue.Split(',');
+        if (parts.Length == 3 &&
+            int.TryParse(parts[0], out int red) &&
+            int.TryParse(parts[1], out int green) &&
+            int.TryParse(parts[2], out int blue))
+        {
+            if (red < 0 || red > 255 || green < 0 || green > 255 || blue < 0 || blue > 255)
+                return;
 
-			ColorPreview.Color = new Color(_currentRed, _currentGreen, _currentBlue);
-			PreviewFrame.BackgroundColor = ColorPreview.Color;
-		}
-	}
+            RedSlider.Value = red;
+            GreenSlider.Value = green;
+            BlueSlider.Value = blue;
 
-	private void OnColorValueSliderChanged(object sender, ValueChangedEventArgs e)
-	{
-		SetColors((int)RedSlider.Value, (int)GreenSlider.Value, (int)BlueSlider.Value);
+            _currentColor = new Color(red, green, blue);
+            SetColorsToPage();
+        }
+    }
 
-		ColorPreview.Color = new Color(_currentRed, _currentGreen, _currentBlue);
-		PreviewFrame.BackgroundColor = ColorPreview.Color;
+    private void OnColorValueSliderChanged(object sender, ValueChangedEventArgs e)
+    {
+        if (!_setupComplete)
+            return;
 
-		UpdateColorValueEntry();
-	}
+        _currentColor = _currentColor = new Color((int)RedSlider.Value, (int)GreenSlider.Value, (int)BlueSlider.Value);
 
-	private void OnBorderThicknessSliderValueChanged(object sender, ValueChangedEventArgs e)
-	{
-		if (sender is Slider slider)
-		{
-			_currentBorderThickness = (int)slider.Value;
-			BorderThicknessLabel.Text = $"{_currentBorderThickness:F0} px";
+        UpdateColorValueEntry();
+        SetColorsToPage();
+    }
 
-			if (_imageWidth > _imageHeight)
-			{
-				var padding = new Thickness(_currentBorderThickness, 0);
-				PreviewFrame.Padding = padding;
-			}
-			else if (_imageWidth < _imageHeight)
-			{
-				var padding = new Thickness(0, _currentBorderThickness);
-				PreviewFrame.Padding = padding;
-			}
-			else
-			{
-				PreviewFrame.Padding = new Thickness(_currentBorderThickness);
-			}
-		}
-	}
+    private void OnBorderThicknessSliderValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        if (sender is Slider slider)
+        {
+            _currentBorderThickness = (int)slider.Value;
+            BorderThicknessLabel.Text = $"{_currentBorderThickness:F0} px";
 
-	private async void OnHomeClicked(object sender, EventArgs e)
-	{
-		await Navigation.PushAsync(new MainPage());
-	}
+            if (_imageWidth > _imageHeight)
+            {
+                var padding = new Thickness(_currentBorderThickness, 0);
+                PreviewFrame.Padding = padding;
+            }
+            else if (_imageWidth < _imageHeight)
+            {
+                var padding = new Thickness(0, _currentBorderThickness);
+                PreviewFrame.Padding = padding;
+            }
+            else
+            {
+                PreviewFrame.Padding = new Thickness(_currentBorderThickness);
+            }
+        }
+    }
 
-	private void OnExportImagesClicked(object sender, EventArgs e)
-	{
+    private async void OnHomeClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MainPage());
+    }
 
-	}
+    private void OnExportImagesClicked(object sender, EventArgs e)
+    {
 
-	private void OnExitClicked(object sender, EventArgs e)
-	{
-		Application.Current.Quit();
-	}
+    }
 
-	private async void PreviousPictureButtonClicked(object sender, EventArgs e)
-	{
-		_currentImageNumber--;
-		await Navigation.PushAsync(new EditImagePage(_imagePaths, _currentImageNumber, _currentBorderThickness, _currentRed, _currentGreen, _currentBlue));
-	}
-	private async void NextPictureButtonClicked(object sender, EventArgs e)
-	{
-		_currentImageNumber++;
-		await Navigation.PushAsync(new EditImagePage(_imagePaths, _currentImageNumber, _currentBorderThickness, _currentRed, _currentGreen, _currentBlue));
-	}
+    private void OnExitClicked(object sender, EventArgs e)
+    {
+        Application.Current.Quit();
+    }
+
+    private async void PreviousPictureButtonClicked(object sender, EventArgs e)
+    {
+        _currentImageNumber--;
+        await Navigation.PushAsync(new EditImagePage(_imagePaths, _currentImageNumber, _currentBorderThickness, _currentColor));
+    }
+    private async void NextPictureButtonClicked(object sender, EventArgs e)
+    {
+        _currentImageNumber++;
+        await Navigation.PushAsync(new EditImagePage(_imagePaths, _currentImageNumber, _currentBorderThickness, _currentColor));
+    }
 }
--- ImageBorderTool2/MainPage.xaml.cs
+++ ImageBorderTool2/MainPage.xaml.cs
@@ -1,147 +1,148 @@
 ﻿namespace ImageBorderTool2
 {
-	public partial class MainPage : ContentPage
-	{
-		private int _imageCount = 0;
-		private List<string> _selectedImagePaths = new List<string>();
+    public partial class MainPage : ContentPage
+    {
+        private int _imageCount = 0;
+        private List<string> _selectedImagePaths = new List<string>();
 
-		public MainPage()
-		{
-			InitializeComponent();
-		}
+        public MainPage()
+        {
+            InitializeComponent();
+        }
 
-		private async void OnAddImagesClicked(object sender, EventArgs e)
-		{
-			var files = await PickImagesAsync();
-			if (files != null)
-			{
-				foreach (var file in files)
-				{
-					AddImageToGrid(file);
-				}
-			}
-		}
+        private async void OnAddImagesClicked(object sender, EventArgs e)
+        {
+            var files = await PickImagesAsync();
+            if (files != null)
+            {
+                foreach (var file in files)
+                {
+                    AddImageToGrid(file);
+                }
+            }
+        }
 
-		private async Task<IEnumerable<FileResult>> PickImagesAsync()
-		{
-			var result = await FilePicker.PickMultipleAsync(new PickOptions
-			{
-				FileTypes = FilePickerFileType.Images,
-				PickerTitle = "Select Images"
-			});
+        private async Task<IEnumerable<FileResult>> PickImagesAsync()
+        {
+            var result = await FilePicker.PickMultipleAsync(new PickOptions
+            {
+                FileTypes = FilePickerFileType.Images,
+                PickerTitle = "Select Images"
+            });
 
-			return result;
-		}
+            return result;
+        }
 
-		private void AddImageToGrid(FileResult file)
-		{
-			var removeButtonSize = 24;
+        private void AddImageToGrid(FileResult file)
+        {
+            var removeButtonSize = 24;
 
-			if (file == null) return;
+            if (file == null) return;
 
-			_selectedImagePaths.Add(file.FullPath);
+            _selectedImagePaths.Add(file.FullPath);
 
-			var imageSource = ImageSource.FromFile(file.FullPath);
+            var imageSource = ImageSource.FromFile(file.FullPath);
 
-			var image = new Image
-			{
-				Source = imageSource,
-				Aspect = Aspect.AspectFit
-			};
+            var image = new Image
+            {
+                Source = imageSource,
+                Aspect = Aspect.AspectFit
+            };
 
-			var removeButton = new Button
-			{
-				Text = "X",
-				BackgroundColor = (Color)Application.Current.Resources["Warning"],
-				TextColor = (Color)Application.Current.Resources["PrimaryText"],
-				FontSize = 16,
-				HorizontalOptions = LayoutOptions.End,
-				VerticalOptions = LayoutOptions.Start,
-				Padding = new Thickness(0),
-				HeightRequest = removeButtonSize,
-				WidthRequest = removeButtonSize,
-				CornerRadius = removeButtonSize / 2,
-			};
+            var removeButton = new Button
+            {
+                Text = "X",
+                BackgroundColor = (Color)Application.Current.Resources["Warning"],
+                TextColor = (Color)Application.Current.Resources["PrimaryText"],
+                FontSize = 16,
+                HorizontalOptions = LayoutOptions.End,
+                VerticalOptions = LayoutOptions.Start,
+                Padding = new Thickness(0),
+                HeightRequest = removeButtonSize,
+                WidthRequest = removeButtonSize,
+                CornerRadius = removeButtonSize / 2,
+            };
 
-			var frame = new Frame
-			{
-				BackgroundColor = (Color)Application.Current.Resources["Gray300"],
-				CornerRadius = 4,
-				HasShadow = false,
-				Margin = new Thickness(8),
-				Padding = 0,
-				Content = new Grid
-				{
-					Children = { image, removeButton }
-				}
-			};
+            var frame = new Frame
+            {
+                BackgroundColor = (Color)Application.Current.Resources["Gray300"],
+                CornerRadius = 4,
+                HasShadow = false,
+                Margin = new Thickness(8),
+                Padding = 0,
+                Content = new Grid
+                {
+                    Children = { image, removeButton }
+                }
+            };
 
-			Grid.SetRow(removeButton, 0);
-			Grid.SetColumn(removeButton, 1);
+            Grid.SetRow(removeButton, 0);
+            Grid.SetColumn(removeButton, 1);
 
-			removeButton.Clicked += (s, e) => RemoveImageFromGrid(frame, file.FullPath);
+            removeButton.Clicked += (s, e) => RemoveImageFromGrid(frame, file.FullPath);
 
-			ImageGrid.Children.Add(frame);
+            ImageGrid.Children.Add(frame);
 
-			int column = _imageCount % 3;
-			int row = _imageCount / 3;
+            int column = _imageCount % 3;
+            int row = _imageCount / 3;
 
-			Grid.SetColumn(frame, column);
-			Grid.SetRow(frame, row);
+            Grid.SetColumn(frame, column);
+            Grid.SetRow(frame, row);
 
-			if (column == 0)
-			{
-				ImageGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
-			}
+            if (column == 0)
+            {
+                ImageGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+            }
 
-			_imageCount++;
+            _imageCount++;
 
-			UpdateImageBoxVisibility();
-			UpdateNextButtonVisibility();
-		}
+            UpdateImageBoxVisibility();
+            UpdateNextButtonVisibility();
+        }
 
-		private void RemoveImageFromGrid(Frame frame, string filePath)
-		{
-			ImageGrid.Children.Remove(frame);
-			_selectedImagePaths.Remove(filePath);
-			_imageCount--;
-			UpdateImageBoxVisibility();
-			UpdateNextButtonVisibility();
-			UpdateImageGrid();
-		}
+        private void RemoveImageFromGrid(Frame frame, string filePath)
+        {
+            ImageGrid.Children.Remove(frame);
+            _selectedImagePaths.Remove(filePath);
+            _imageCount--;
+            UpdateImageBoxVisibility();
+            UpdateNextButtonVisibility();
+            UpdateImageGrid();
+        }
 
-		private void UpdateImageGrid()
-		{
-			ImageGrid.RowDefinitions.Clear();
+        private void UpdateImageGrid()
+        {
+            ImageGrid.RowDefinitions.Clear();
 
-			for (int i = 0; i < ImageGrid.Children.Count; i++)
-			{
-				var frame = (Frame)ImageGrid.Children[i];
+            for (int i = 0; i < ImageGrid.Children.Count; i++)
+            {
+                var frame = (Frame)ImageGrid.Children[i];
 
-				int column = i % 3;
-				int row = i / 3;
+                int column = i % 3;
+                int row = i / 3;
 
-				Grid.SetColumn(frame, column);
-				Grid.SetRow(frame, row);
+                Grid.SetColumn(frame, column);
+                Grid.SetRow(frame, row);
 
-				if (column == 0)
-				{
-					ImageGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
-				}
-			}
-		}
+                if (column == 0)
+                {
+                    ImageGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+                }
+            }
+        }
 
-		private void UpdateImageBoxVisibility() => ImageBox.IsVisible = _imageCount > 0;
-		private void UpdateNextButtonVisibility() => ContinueButton.IsVisible = _imageCount > 0;
+        private void UpdateImageBoxVisibility() => ImageBox.IsVisible = _imageCount > 0;
+        private void UpdateNextButtonVisibility() => ContinueButton.IsVisible = _imageCount > 0;
 
-		private void OnExitClicked(object sender, EventArgs e)
-		{
-			Application.Current.Quit();
-		}
+        private void OnExitClicked(object sender, EventArgs e)
+        {
+            Application.Current.Quit();
+        }
 
-		private async void OnContinueClicked(object sender, EventArgs e)
-		{
-			await Navigation.PushAsync(new EditImagePage(_selectedImagePaths, 0, 10, 200, 100, 50));
-		}
-	}
+        private async void OnContinueClicked(object sender, EventArgs e)
+        {
+            var defaultColor = new Color(255, 255, 255);
+            await Navigation.PushAsync(new EditImagePage(_selectedImagePaths, 0, 10, defaultColor));
+        }
+    }
 }
